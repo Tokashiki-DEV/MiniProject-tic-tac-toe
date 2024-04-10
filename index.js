031e540e0b97d6c3aff37e9563e32bdfbc717f88
Marcos Tokashiki <marcostokashiki@gmail.com>
2024-04-10 16:52:10 -0400

    Adicionando funcionalidades

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const main = document.querySelector("main")
 const buttons = document.querySelectorAll(".gameBtn")
-
-
+let player = document.querySelector('h1#player')
+let plays = 0
+game()
 function game(){
 
+    buttons.forEach(function(pressed){
+        pressed.addEventListener('click',playerpressed)
+    })
+    
+}
+
+function playerpressed(){
+    if(player.dataset.player === 'Player1'){
+        this.innerText = 'X'
+        player.dataset.player = 'Player2'
+    }else{
+        this.innerText = 'O'
+        player.dataset.player = 'Player1'
+    }
 }
