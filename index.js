dd4d30d25f56d1e6f864d833e9ed65d019fbb737
Marcos Tokashiki <marcostokashiki@gmail.com>
2024-04-11 15:46:10 -0400

    tentando incluir pra quem ganha

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const main = document.querySelector("main")
 const buttons = document.querySelectorAll(".gameBtn")
 let player = document.querySelector('h1#player')
 let plays = 0
+let i = 0
 game()
 function game(){
 
     buttons.forEach(function(pressed){
         pressed.addEventListener('click',playerpressed)
     })
+
+    
     
 }
 
 function playerpressed(){
+
     if(player.dataset.player === 'Player1'){
         this.innerText = 'X'
         player.dataset.player = 'Player2'
@@ -19,4 +23,24 @@ function playerpressed(){
         this.innerText = 'O'
         player.dataset.player = 'Player1'
     }
+}
+
+function winCondition(){
+   buttons.forEach(function(){
+    let column = buttons[i].dataset.column
+    let row = buttons[i].dataset.row
+
+    switch(column){
+        case 'left':
+        if(buttons[i].innerText === 'X'){
+            console.log(buttons[i].innerText + ' Ganhou')
+        }else if (buttons[i].innerText === 'O'){
+            console.log(buttons[i].innerText + ' Ganhou')
+        }
+        break
+    }
+
+
+    i++ 
+   })
 }
