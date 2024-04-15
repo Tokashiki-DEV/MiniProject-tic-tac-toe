c18bcd57d5096d784607a76262b6851d0b79f4ad
Marcos Tokashiki <marcostokashiki@gmail.com>
2024-04-15 15:17:52 -0400

    incluindo fim de jogo
    agora mostra o jogo sendo finalizado e quem ganhou
    
    falta adicionar a opção de empate e mostrar qual foi a condição que ganhou

--- index.js
+++ index.js
@@ -1,101 +1,127 @@
 const main = document.querySelector("main")
 const buttons = document.querySelectorAll(".gameBtn")
 let player = document.querySelector('h1#player')
-let plays = 0
 let i = 0
+let j = 0
 let column = buttons[i].dataset.column
 let row = buttons[i].dataset.row
 let cross = buttons[i].dataset.cross
 
 let player1 = prompt("Digite o nome do jogador 1: ")
-let Player2 = prompt("Digite o nome do jogador 2: ")
+player.dataset.player = player1
+let player2 = prompt("Digite o nome do jogador 2: ")
+player.innerText = 'Turno do ' + player2
 
 game()
 function game(){
 
-    buttons.forEach(function(pressed){
-        pressed.addEventListener('click',playerpressed)
-        pressed.addEventListener('click',winCondition)
-    })
+
+        buttons.forEach(function(pressed){
+            pressed.addEventListener('click',playerpressed)
+            pressed.addEventListener('click',winCondition)
+            pressed.addEventListener('click',update)
+        })
+    
 
     
     
 }
-
+function update(){
+    switch(winCondition()){
+        case undefined:
+            break
+        case 'X Ganhou':
+            buttons.forEach(function(){
+                buttons[j].disabled = true
+                j++
+            })
+            player.innerText = player.dataset.player + ' ganhou'
+            break
+        case 'O Ganhou':
+            buttons.forEach(function(){
+                buttons[j].disabled = true
+                j++
+            })
+            player.innerText = player.dataset.player + ' ganhou'
+            break
+    }
+}
 
 
 function playerpressed(){
-    if(player.dataset.player === 'Player1'){
+    if(player.dataset.player === player2 && this.innerText === ''){
         this.innerText = 'X'
-        player.dataset.player = 'Player2'
-    }else{
+        player.innerText = 'Turno do ' + player2
+        player.dataset.player = player1
+    }else if(player.dataset.player === player1 && this.innerText === ''){
         this.innerText = 'O'
-        player.dataset.player = 'Player1'
+        player.innerText = 'Turno do ' + player1
+        player.dataset.player = player2
     }
 }
 
 function winCondition(){
-   buttons.forEach(function(){
 
+  
     switch(column){
         case 'left':
         if(buttons[0].innerText === 'X' && buttons[3].innerText === 'X' && buttons[6].innerText === 'X'){
-            console.log('X Ganhou')
-            break
+            
+            return ('X Ganhou')
+
         }else if (buttons[0].innerText === 'O' && buttons[3].innerText === 'O' && buttons[6].innerText === 'O'){
-            console.log('O Ganhou')
-            break
+            return 'O Ganhou'
         }
         
 
         case 'middle':
         if(buttons[1].innerText === 'X' && buttons[4].innerText === 'X' && buttons[7].innerText === 'X'){
-            console.log('X Ganhou')
-            break
+            return 'X Ganhou'
+            
         }else if (buttons[1].innerText === 'O' && buttons[4].innerText === 'O' && buttons[7].innerText === 'O'){
-            console.log('O Ganhou')
-            break
+            return 'O Ganhou'
+            
         }
         
 
         case 'right':
         if(buttons[2].innerText === 'X' && buttons[5].innerText === 'X' && buttons[8].innerText === 'X'){
-            console.log('X Ganhou')
-            break
+            return 'X Ganhou'
+        
         }else if (buttons[2].innerText === 'O' && buttons[5].innerText === 'O' && buttons[8].innerText === 'O'){
-            console.log('O Ganhou')
-            break
+            return 'O Ganhou'
+            
         }
         
     }
     switch(row){
         case 'top':
         if(buttons[0].innerText === 'X' && buttons[1].innerText === 'X' && buttons[2].innerText === 'X'){
-            console.log('X Ganhou')
-            break
+            return 'X Ganhou'
+            
         }else if (buttons[0].innerText === 'O' && buttons[1].innerText === 'O' && buttons[2].innerText === 'O'){
-            console.log('O Ganhou')
-            break
+            return 'O Ganhou'
+            
         }
         
 
         case 'mid':
         if(buttons[3].innerText === 'X' && buttons[4].innerText === 'X' && buttons[5].innerText === 'X'){
-            console.log('X Ganhou')
-            break
+            return 'X Ganhou'
+            
         }else if (buttons[3].innerText === 'O' && buttons[4].innerText === 'O' && buttons[5].innerText === 'O'){
-            console.log('O Ganhou')
-            break
+            return 'O Ganhou'
+            
         }
        
 
         case 'bot':
         if(buttons[6].innerText === 'X' && buttons[7].innerText === 'X' && buttons[8].innerText === 'X'){
-            console.log('X Ganhou')
-            break
+            return 'X Ganhou'
+            
         }else if (buttons[6].innerText === 'O' && buttons[7].innerText === 'O' && buttons[8].innerText === 'O'){
-            console.log('O Ganhou')
-            break
+            return 'O Ganhou'
+            
         }
         
     }
@@ -103,23 +129,21 @@ function winCondition(){
         case 'left':
         case 'middle':
         if(buttons[0].innerText === 'X' && buttons[4].innerText === 'X' && buttons[8].innerText === 'X'){
-            console.log('X Ganhou')
-            break
+            return 'X Ganhou'
+            
         }else if(buttons[0].innerText === 'O' && buttons[4].innerText === 'O' && buttons[8].innerText === 'O'){
-            console.log('O Ganhou')
-            break
+            return 'O Ganhou'
+            
         }
         case 'right':
         case 'middle':
         if(buttons[2].innerText === 'X' && buttons[4].innerText === 'X' && buttons[6].innerText === 'X'){
-            console.log('X Ganhou')
-            break
+            return 'X Ganhou'
+            
         }else if(buttons[2].innerText === 'O' && buttons[4].innerText === 'O' && buttons[6].innerText === 'O'){
-            console.log('O Ganhou')
-            break
+            return 'O Ganhou'
+            
         }
     }
     i++ 
-
-   })
 }
